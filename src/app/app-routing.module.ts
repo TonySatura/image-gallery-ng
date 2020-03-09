a42d7d16d2786478cca3f9abdcb39d98652abdb2
TonySatura <tony.satura@awin.com>
2020-03-09 13:15:03 +0100

    Adds the components and their routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
+import { DetailsComponent } from './details/details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '**', redirectTo: 'overview', pathMatch: 'full' },
+    {
+        path: 'overview',
+        component: OverviewComponent,
+        data: { pageTitle: 'Overview' }
+    },
+    {
+        path: 'album/:title',
+        component: DetailsComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
