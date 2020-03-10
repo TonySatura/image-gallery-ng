bea5885414ce65a5c8b6cd975a8688aa70a514ed
TonySatura <tony.satura@awin.com>
2020-03-10 20:02:56 +0100

    Adds aws-sdk

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AlbumService } from '../services/album.service';
 
 @Component({
-  selector: 'app-overview',
-  templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+    selector: 'app-overview',
+    templateUrl: './overview.component.html',
+    styleUrls: ['./overview.component.scss']
 })
 export class OverviewComponent implements OnInit {
+    albums: string[];
 
-  constructor() { }
+    constructor(private albumService: AlbumService) {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit() {
+        this.albumService
+            .listAlbums()
+            .subscribe(albums => (this.albums = albums));
+    }
 }
--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@angular/core';
+import * as AWS from 'aws-sdk/global';
+import * as S3 from 'aws-sdk/clients/s3';
+import { Observable, from } from 'rxjs';
+import { map, tap, catchError, flatMap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AlbumService {
-    constructor() {}
+    private albumBucketName = 'tonomony-images';
+    private albumBucket: S3;
+
+    constructor() {
+        // Initialize the Amazon Cognito credentials provider
+        AWS.config.region = 'eu-central-1'; // Region
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'eu-central-1:9b02f2ec-86ff-4ea1-b697-fab841a603b0'
+        });
+
+        // Create a new service object
+        this.albumBucket = new S3({
+            apiVersion: '2006-03-01',
+            params: { Bucket: this.albumBucketName }
+        });
+    }
+
+    // A utility function to create HTML.
+    private getHtml(template) {
+        return template.join('\n');
+    }
+
+    // List the photo albums that exist in the bucket.
+    public listAlbums(): Observable<string[]> {
+        const params = { Delimiter: '/' } as S3.ListObjectsRequest;
+
+        const promise = this.albumBucket.listObjects(params).promise();
+
+        return from(promise).pipe(
+            map(listObjects =>
+                listObjects.CommonPrefixes.map(commonPrefixes =>
+                    decodeURIComponent(commonPrefixes.Prefix.replace('/', ''))
+                )
+            )
+        );
+    }
 }
