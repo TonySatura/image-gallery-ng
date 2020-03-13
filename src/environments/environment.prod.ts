ac520a1a2fe7caf5b7fa6fdec53f8d845b12c85f
TonySatura <tony.satura@awin.com>
2020-03-13 16:24:29 +0100

    Tries to fix build issues with AWS CodeBuild.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+    production: true,
+    aws: {
+        region: 'eu-central-1',
+        identityPoolId: 'eu-central-1:9b02f2ec-86ff-4ea1-b697-fab841a603b0'
+    }
 };
