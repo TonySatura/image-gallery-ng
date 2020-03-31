2b5b97d941336a96cff68627215a7243ad0af186
TonySatura <tony.satura@awin.com>
2020-03-31 11:39:35 +0200

    Updates environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
     production: false,
     album: {
-        bucketName: 'tonomony-images',
-        imageHandlerEndpoint: 'https://d3j7rohb8t3esh.cloudfront.net/'
+        bucketName: 'image-gallery-images-bucket43879c71-144tb5q75m5ai',
+        imageHandlerEndpoint: 'https://d3j7rohb8t3esh.cloudfront.net'
     },
     aws: {
         region: 'eu-central-1',
-        identityPoolId: 'eu-central-1:9b02f2ec-86ff-4ea1-b697-fab841a603b0'
+        identityPoolId: 'eu-central-1:0b698247-b5cd-4975-a9cc-6db279d41a9a'
     }
 };
 
