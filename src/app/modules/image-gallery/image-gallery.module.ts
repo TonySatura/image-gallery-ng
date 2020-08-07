26c636c2b04259a703f1faccfea126601d1635b2
TonySatura <tony.satura@awin.com>
2020-08-07 13:23:09 +0200

    Resolves merge conflicts

--- src/app/modules/image-gallery/image-gallery.module.ts
+++ src/app/modules/image-gallery/image-gallery.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './components/details/details.component';
 import { OverviewComponent } from './components/overview/overview.component';
-import { CacheModule } from '../cache/cache.module';
 import { ImageGalleryRoutingModule } from './image-gallery-routing.module';
 import { AwsModule } from '../aws/aws.module';
-<<<<<<< HEAD
-import { CacheService } from '../shared/services/cache.service';
-import { CACHE_SETTINGS } from '../shared/services/cache.config';
-import { CacheSettings } from '../shared/models/cache.model';
-=======
 import { CacheService } from '../cache/services/cache.service';
+import { CacheSettings } from '../cache/models/cache.model';
 import { CACHE_SETTINGS } from '../cache/services/cache.config';
-import { CacheSettings, CacheStorageType } from '../cache/models/cache.model';
->>>>>>> 6eb328b002cf32492e8249951b32c9f2a86fdc56
 import { environment } from 'src/environments/environment';
 
 const IG_CACHE_SETTINGS: CacheSettings = {
@@ -24,7 +17,7 @@ const IG_CACHE_SETTINGS: CacheSettings = {
 };
 
 @NgModule({
-    imports: [AwsModule, CommonModule, ImageGalleryRoutingModule, CacheModule],
+    imports: [AwsModule, CommonModule, ImageGalleryRoutingModule],
     declarations: [DetailsComponent, OverviewComponent],
     providers: [
         CacheService,
